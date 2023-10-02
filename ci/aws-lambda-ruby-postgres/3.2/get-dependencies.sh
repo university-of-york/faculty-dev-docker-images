ccdf30d802ff87f0afe110b8490c74a96aff1a5a
Dan Miller <dan.miller@york.ac.uk>
2023-10-02 15:35:52 +0100

    Add Ruby 3.2 images

---
+++ ci/aws-lambda-ruby-postgres/3.2/get-dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Gets a zip file for the given lambda layer ARN
+
+LAYER_ARN=arn:aws:lambda:eu-west-1:326340845860:layer:sys-layer-ruby-pg:5
+URL=$(aws lambda get-layer-version-by-arn --arn $LAYER_ARN --query Content.Location --output text)
+curl -s $URL -o sys-layer-ruby-pg.zip
+
+# Convert the zip to tgz so docker can ADD the archive automatically
+
+unzip -q sys-layer-ruby-pg.zip -d layer
+tar -C layer -zcf sys-layer-ruby-pg.tgz .
+rm -rf layer sys-layer-ruby-pg.zip
