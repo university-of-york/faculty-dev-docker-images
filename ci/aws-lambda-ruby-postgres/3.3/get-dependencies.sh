f6b8aa6819547c4da2c81e360e2d1fbe733965c4
mjp513 <matthew.pickin@york.ac.uk>
2024-10-24 17:09:21 +0100

    Add new Dockerfiles for Ruby 3.3 images

---
+++ ci/aws-lambda-ruby-postgres/3.3/get-dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Gets a zip file for the given lambda layer ARN
+
+# Dev SYS AWS account - use production ARN when available
+LAYER_ARN=arn:aws:lambda:eu-west-1:777293634910:layer:sys-layer-ruby-pg:1
+URL=$(aws lambda get-layer-version-by-arn --arn $LAYER_ARN --query Content.Location --output text)
+curl -s $URL -o sys-layer-ruby-pg.zip
+
+# Convert the zip to tgz so docker can ADD the archive automatically
+
+unzip -q sys-layer-ruby-pg.zip -d layer
+tar -C layer -zcf sys-layer-ruby-pg.tgz .
+rm -rf layer sys-layer-ruby-pg.zip
