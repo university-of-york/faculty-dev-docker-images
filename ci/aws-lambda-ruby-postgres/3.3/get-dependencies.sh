14a0a20ac2ee8153fc19dc2fc63040850cb13526
mjp513 <matthew.pickin@york.ac.uk>
2024-10-28 11:05:57 +0000

    Add a TODO tag to comment

--- ci/aws-lambda-ruby-postgres/3.3/get-dependencies.sh
+++ ci/aws-lambda-ruby-postgres/3.3/get-dependencies.sh
@@ -2,7 +2,7 @@
 
 # Gets a zip file for the given lambda layer ARN
 
-# Dev SYS AWS account - use production ARN when available
+# TODO: Dev SYS AWS account - use production ARN when available
 LAYER_ARN=arn:aws:lambda:eu-west-1:777293634910:layer:sys-layer-ruby-pg:1
 URL=$(aws lambda get-layer-version-by-arn --arn $LAYER_ARN --query Content.Location --output text)
 curl -s $URL -o sys-layer-ruby-pg.zip
