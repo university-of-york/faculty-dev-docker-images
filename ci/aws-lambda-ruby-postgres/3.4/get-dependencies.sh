a0f0eea9cfd071fb22b3aed399591a311e12d2f9
mjp513 <matthew.pickin@york.ac.uk>
2025-11-20 14:39:55 +0000

    Add new Dockerfiles targeting ruby 3.4
    - Make 3.4-specific changes to Dockerfiles, where
      applicable
    - Fetch 3.4 lambda layer from mjp513 sandbox AWS
      account
    
    fd-2030

---
+++ ci/aws-lambda-ruby-postgres/3.4/get-dependencies.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Gets a zip file for the given lambda layer ARN
+
+# TODO: FD-2030 - Dev SYS AWS account - use production ARN when available
+# This is from mjp513 sandbox account
+LAYER_ARN=arn:aws:lambda:eu-west-1:787901675629:layer:my-layer:1
+URL=$(aws lambda get-layer-version-by-arn --arn $LAYER_ARN --query Content.Location --output text)
+curl -s $URL -o sys-layer-ruby-pg.zip
+
+# Convert the zip to tgz so docker can ADD the archive automatically
+
+unzip -q sys-layer-ruby-pg.zip -d layer
+tar -C layer -zcf sys-layer-ruby-pg.tgz .
+rm -rf layer sys-layer-ruby-pg.zip
