ca45033222036329b3a6d0b3a6b6ef797a392452
mjp513 <matthew.pickin@york.ac.uk>
2025-12-16 11:10:02 +0000

    Bump specified layer version again for Ruby 3.4
    Bump from 3 to 4
    
    fd-2030

--- ci/aws-lambda-ruby-postgres/3.4/get-dependencies.sh
+++ ci/aws-lambda-ruby-postgres/3.4/get-dependencies.sh
@@ -4,7 +4,7 @@
 
 # TODO: FD-2030 - Dev SYS AWS account - use production ARN when available
 # This is from mjp513 sandbox account
-LAYER_ARN=arn:aws:lambda:eu-west-1:787901675629:layer:my-layer:3
+LAYER_ARN=arn:aws:lambda:eu-west-1:787901675629:layer:my-layer:4
 # LAYER_ARN=arn:aws:lambda:eu-west-1:326340845860:layer:sys-layer-ruby-pg:8
 URL=$(aws lambda get-layer-version-by-arn --arn $LAYER_ARN --query Content.Location --output text)
 curl -s $URL -o sys-layer-ruby-pg.zip
